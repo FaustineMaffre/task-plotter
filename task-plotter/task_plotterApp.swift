7916efc2a8757abdb191782302bef9ccd458e0bf
faustinemaffre <faustine.maffre@gmail.com>
2020-12-07 11:34:57 +0100

    Initial Commit

---
+++ task-plotter/task_plotterApp.swift
@@ -0,0 +1,17 @@
+//
+//  task_plotterApp.swift
+//  task-plotter
+//
+//  Created by Faustine Maffre on 07/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct task_plotterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
